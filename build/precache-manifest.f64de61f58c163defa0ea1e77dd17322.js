11a5c0f8a7e1611ec1be54d29bc1fdc8aab7e77e
Vivi Tseng <vii0726@gmail.com>
2019-10-03 17:39:37 +0800

    build files

---
+++ build/precache-manifest.f64de61f58c163defa0ea1e77dd17322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa331500749c71b9da57fb3404629c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cb727b3f55698c6e62d",
+    "url": "./static/css/main.0b15a019.chunk.css"
+  },
+  {
+    "revision": "af15c4487ff3b20eeb10",
+    "url": "./static/js/2.49b57b08.chunk.js"
+  },
+  {
+    "revision": "0cb727b3f55698c6e62d",
+    "url": "./static/js/main.4a9ba46c.chunk.js"
+  },
+  {
+    "revision": "fdf69b96e788c230c746",
+    "url": "./static/js/runtime-main.4e4c60e2.js"
+  }
+]);
